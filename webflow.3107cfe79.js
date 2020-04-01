de2154bd6db590357c933babf591afb10560bf3c
Vanshbordia <vanshbordia@gmail.com>
2020-04-02 02:26:16 +0530

    1.4.5

--- webflow.3107cfe79.js
+++ webflow.3107cfe79.js
@@ -459,7 +459,7 @@
         return n.ready = function() {
             var n, i, a, s = o.attr("data-wf-status"),
                 h = o.attr("data-wf-domain") || "";
-            /\.webflow\.io$/i.test(h) && u.hostname !== h && (s = !0), s && !c && (e = e || (n = t('<a class="w-webflow-badge"></a>').attr("href", ""), i = t("<img>").attr("src", "").attr("alt", "").css({ marginRight: "8px", width: "16px" }), a = t("<img>").attr("src", "").attr("alt", "Made in Webflow"), n.append(i, a), n[0]), d(), setTimeout(d, 500), t(r).off(l, f).on(l, f))
+            /\.webflow\.io$/i.test(h) && u.hostname !== h && (s = !0), s && !c &&
         }, n
     })
 }, function(t, e, n) {
